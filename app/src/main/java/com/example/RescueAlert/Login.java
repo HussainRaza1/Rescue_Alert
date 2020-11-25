0f186c1eeabca95b895d34cc8fc2f95da827bb61
LeonRaza <73407875+LeonRaza@users.noreply.github.com>
2020-11-26 02:17:14 +0500

    small fix

--- app/src/main/java/com/example/RescueAlert/Login.java
+++ app/src/main/java/com/example/RescueAlert/Login.java
@@ -8,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
-import com.google.android.gms.maps.model.Dash;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
